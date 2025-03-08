a1323a7c276569329ec7296c00947536ecd1d922
ziadrad <ziadrady60@gmail.com>
2025-03-08 20:38:34 +0200

    Add Department Page Functions

---
+++ assignement_3.PL/dto/CreatDepartmentDto.cs
@@ -0,0 +1,20 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace assignement_3.PL.dto
+{
+    public class CreatDepartmentDto
+    {
+        [Required(ErrorMessage = " Code is Required !")]
+
+        public string Code { get; set; }
+
+        [Required(ErrorMessage = " Name is Required !")]
+
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = " CreateAt is Required !")]
+
+        public DateTime CreateAt { get; set; }
+    }
+}
